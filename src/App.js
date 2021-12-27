b123e08896eb6984372c4fad7c3b1e0afbee401e
Zulharmin-192113301 <192113301@students.mikroskil.ac.id>
2021-12-27 20:36:29 +0700

    Latihan 3.5 - komentar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
 
     return(
       <div className='isi'>
-          <h1 style={{textAlign:'center', color:'rgb(252, 150, 120)'}}>Hello World</h1>
+          {/* <h1 style={{textAlign:'center', color:'rgb(252, 150, 120)'}}>Hello World</h1> */}
           <h2 className='subjudul'>Praktek App Web</h2>
           <p className='paragraf'>Ini adalah contoh paragraf nya ya cuy</p>
       </div>
