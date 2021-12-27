04d8748e59902b8da9efd3325639c5e5c74aee09
Zulharmin-192113301 <192113301@students.mikroskil.ac.id>
2021-12-27 20:38:56 +0700

    Latihan 3.6 - deklarasi variabel

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ class App extends Component {
 
   render() {
 
+    var judul = <u>Johny</u>;
+    const vsubjudul =  "Praktek App Website";
+    this.tulisan = "tes paragraf lagi boleh lah ya";
+
     return(
       <div className='isi'>
-          {/* <h1 style={{textAlign:'center', color:'rgb(252, 150, 120)'}}>Hello World</h1> */}
-          <h2 className='subjudul'>Praktek App Web</h2>
-          <p className='paragraf'>Ini adalah contoh paragraf nya ya cuy</p>
+          <h1 style={{textAlign:'center', color:'rgb(252, 150, 120)'}}> {judul} </h1>
+          <h2 className='subjudul'>  {vsubjudul} </h2>
+          <p className='paragraf'> {this.tulisan} </p>
       </div>
     );
   }
